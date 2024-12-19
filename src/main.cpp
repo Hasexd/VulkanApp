be2d21371c489c9253f83712cca48a8a60338c20
Hasexd <owojcik1234@gmail.com>
2024-12-19 20:40:43 +0100

    repo clean

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <vulkan/vulkan.h>
+#include <VkBootstrap.h>
+#include <vk_mem_alloc.h>
+
+int main()
+{
+	std::cout << "Hello world!";
+	std::cin.get();
+}
