c2f7c5a0484d2351c2eb93756bab907b015356f4
Oliwier Wójcik <97812659+Hasexd@users.noreply.github.com>
2025-12-19 19:46:30 +0100

    better plane rotation and new scene

--- src/Plane.cpp
+++ src/Plane.cpp
@@ -8,18 +8,21 @@ Plane::Plane(const std::string& name, const glm::vec3& position, const glm::vec3
 
 glm::vec3 Plane::GetNormal() const
 {
-    glm::mat4 rotationMatrix = glm::eulerAngleXYZ(m_Rotation.x, m_Rotation.y, m_Rotation.z);
-    return glm::vec3(rotationMatrix * glm::vec4(0, 1, 0, 0));
+	glm::vec3 radians = glm::radians(m_Rotation);
+	glm::mat4 rotationMatrix = glm::eulerAngleXYZ(radians.x, radians.y, radians.z);
+	return glm::vec3(rotationMatrix * glm::vec4(0, 1, 0, 0));
 }
 
 glm::vec3 Plane::GetTangent() const
 {
-	glm::mat4 rotationMatrix = glm::eulerAngleXYZ(m_Rotation.x, m_Rotation.y, m_Rotation.z);
+	glm::vec3 radians = glm::radians(m_Rotation);
+	glm::mat4 rotationMatrix = glm::eulerAngleXYZ(radians.x, radians.y, radians.z);
 	return glm::vec3(rotationMatrix * glm::vec4(1, 0, 0, 0));
 }
 
 glm::vec3 Plane::GetBitangent() const 
 {
-	glm::mat4 rotationMatrix = glm::eulerAngleXYZ(m_Rotation.x, m_Rotation.y, m_Rotation.z);
+	glm::vec3 radians = glm::radians(m_Rotation);
+	glm::mat4 rotationMatrix = glm::eulerAngleXYZ(radians.x, radians.y, radians.z);
 	return glm::vec3(rotationMatrix * glm::vec4(0, 0, 1, 0));
 }
